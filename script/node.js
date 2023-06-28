2f03178cca8a79ba00e93f4d3dd7abbb47415f0e
Gleidiston JR <81264980+GleidistonJR@users.noreply.github.com>
2023-06-28 12:38:11 -0300

    Add files via upload

--- script/node.js
+++ script/node.js
@@ -1,18 +1,32 @@
-var barras = document.getElementById('imgBarras')
+var barras = document.getElementById('imgBarras') //criando variavel e atribuindo imagen do HTML
 
-barras.addEventListener('click', painelEsq)
+barras.addEventListener('click', painelEsq) //adicionando evento de click, chama funçao "painelEsq"
 
-function painelEsq() {
-    var cheqbox = document.querySelector('#chec')
-    var painel = document.querySelector('#painel-esq')
+var cheqbox = document.querySelector('#chec') //variavel para checar se checkbox esta habilitada ou nao
 
-    if (cheqbox.checked) {
-        painel.classList.remove('positivo')
+function painelEsq() {  //funçao "painelEsc"
+
+    let painel = document.querySelector('#painel-esq')
+
+    if (cheqbox.checked) {  //se checkbox estiver habilitada atribui class ao elemento painel
+        painel.classList.remove('positivo') //isso fara ele aparecer na tela
         painel.classList.add('negativo')
 
     } else {
-        painel.classList.remove('negativo')
-        painel.classList.add('positivo')
+        painel.classList.remove('negativo') //caso desabilitado remove class
+        painel.classList.add('positivo')  //e fara o painel se esconder a direita!
 
     }
+}
+
+var botaoFiltro = document.querySelector('#BotoesFiltros') //mecanismo para quando clicar em link do filtro..
+botaoFiltro.addEventListener('click', RemoverCheck)     // o painel esconde-se novamente
+
+function RemoverCheck(){
+   cheqbox.checked = false
+
+   let painel = document.querySelector('#painel-esq')
+    painel.classList.remove('positivo') //caso desabilitado remove class
+    painel.classList.add('negativo')  //e fara o painel se esconder a direita!
+
 }
